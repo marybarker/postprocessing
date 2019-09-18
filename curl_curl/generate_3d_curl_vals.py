324576e5e57da4a8589f9c222ad003421fc81800
marybarker <marybarker103@gmail.com>
2019-09-18 13:15:44 -0500

    commit for porting work/home computers. Still work in progress

--- curl_curl/generate_3d_curl_vals.py
+++ curl_curl/generate_3d_curl_vals.py
@@ -11,7 +11,8 @@ print('\ncurl curl V = ', curl(w, R), '\n')
 
 
 
-v = sin(R[0])*sin(R[1])*R.x + sin(R[1])*sin(R[2])*R.y + sin(R[0])*sin(R[2])*R.z
+#v = sin(R[0])*sin(R[1])*R.x + sin(R[1])*sin(R[2])*R.y + sin(R[0])*sin(R[2])*R.z
+v = sin(R[1])*sin(R[2])*R.x + sin(R[0])*sin(R[2])*R.y + sin(R[0])*sin(R[1])*R.z
 print('\nV = ', v)
 print('\ncurl V = ', curl(v, R))
 w = curl(v, R)
